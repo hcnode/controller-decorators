5cc15518528d6a380361dc3befe9d9ee1a0432e1
hcnode <7372@163.com>
2017-07-12 15:15:38 +0800

    skip view route

--- src/bindings.ts
+++ src/bindings.ts
@@ -57,22 +57,23 @@ export function bindRoutes(routerRoutes: any, controllers: any[], getter?: (ctrl
         reactRouters.push({
           component: view, path: url
         });
-      }
-      routerRoutes[method](url, ...middleware, async function (ctx, next) {
-        const inst = getter === undefined ?
-          new ctrl() : getter(ctrl);
+      }else{
+        routerRoutes[method](url, ...middleware, async function (ctx, next) {
+          const inst = getter === undefined ?
+            new ctrl() : getter(ctrl);
 
-        const args = getArguments(params, ctx, next);
-        const result = inst[name](...args);
-        if (response) {
-          response(ctx, await result)
-        } else {
-          if (result) {
-            ctx.body = await result;
+          const args = getArguments(params, ctx, next);
+          const result = inst[name](...args);
+          if (response) {
+            response(ctx, await result)
+          } else {
+            if (result) {
+              ctx.body = await result;
+            }
           }
-        }
-        return result;
-      });
+          return result;
+        });
+      }
     }
   }
   return reactRouters;
